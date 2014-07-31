f6e7c429f6323f32e062ade4c10d2ea33e057c3b
extra.pedestrian@gmail.com <extra.pedestrian@gmail.com@4aa54e81-0bf1-d34a-6337-0c59022f4c98>
2014-07-31 14:28:38 +0000

    you know what? this is starting to be very beautiful. im so excited when i see sockets in action! i cant wait to make second player move in front of my eyes :)

--- trunk/node_coin/index.js
+++ trunk/node_coin/index.js
@@ -1,27 +1,54 @@
-//var app = require('express')();
-
 var express = require('express');
- 
-var server = express();
-server.use(express.static(__dirname + '/coin_game'));
-var http = require('http').Server(server);
-var io = require('socket.io')(http);
-//http.use(app.static(__dirname + '/'));
+var http = require('http');
+var app = express();
+app.use(express.static(__dirname + '/coin_game'));
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
 
-//app.get('/', function(req, res){
-//  res.sendfile('index_coin.html');
-//});
+var games = []
+var newGame = {}
 
 io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
+	console.log('a user connected');
+	//get username
+	var username = "pera";
+	var myGame = games.length;
+	var myNumber = 1;
+	
+	if(newGame.player1 == undefined)
+	{
+		//create new game
+		newGame = {id:games.length, player1: username, player1socket: socket, player2: "", player2socket: null, dateCreated:Date.now(), started:false, playerTurn: username, totalMoves:0};
+	}
+	else
+	{
+		myNumber = 2;
+		newGame.player2 = username;
+		newGame.player2socket = socket;
+		newGame.started = true;
+		//TODO: send game id to both players
+		games.push(newGame);
+		newGame = {};
+		console.log("games:"+games);
+	}
+	
+	socket.on('game moved', function(direction){
+		console.log("game moved, myGame" + myGame + ", myNumber:" + myNumber);
+		if(games[myGame])
+		{
+			if (myNumber == 1)
+				games[myGame].player2socket.emit('oponent move', direction);
+			else
+				games[myGame].player1socket.emit('oponent move', direction);
+		}
+	});
+	
+	socket.on('disconnect', function(){
+		console.log('user disconnected');
+		//TODO:remove from game queue?
+	});
 });
 
 server.listen(3000, function(){
-  console.log('listening on *:3000');
+	console.log('listening on *:3000');
 });
